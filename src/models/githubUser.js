aa74881685fe1928a6ca7427e979c0ce29f9cb35
WuXueqian <wuxueqian2010@hotmail.com>
2017-03-01 13:18:31 +0800

    增加rxdb, 以及babel async/await转化插件

--- src/models/githubUser.js
+++
@@ -1,6 +0,0 @@
-export default class GithubUser {
-  constructor () {
-    username = ''
-    name = ''
-  }
-}
--- src/renderer/actions/accounts.js
+++ src/renderer/actions/accounts.js
@@ -8,7 +8,7 @@ export const getLocalCredentials = (successCb) => {
       typeof successCb === 'function' && // in login page, we need try login if get credentials successfully
       Authentication.signInApp(credentials, (err, profile) => {
         !err && profile && typeof profile === 'object' ? dispatch({type: CONSTANTS.REQUEST_LOGIN_SUCCESS, profile, msg: '', callback: successCb})
-        : dispatch({type: CONSTANTS.REQUEST_LOGIN_FAIL, profile: null, msg: err, callback: successCb})
+        : dispatch({type: CONSTANTS.REQUEST_LOGIN_FAIL, profile: null, msg: err.message, callback: successCb})
       }))
     : dispatch({type: CONSTANTS.GET_LOCAL_CREDENTIALS_FAIL, credentials})
   })
