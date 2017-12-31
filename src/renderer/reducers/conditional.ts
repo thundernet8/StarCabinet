55ffc9ba89ecb17843b3a044d8bbebe6e7aa0ef9
wuxueqian <wuxueqian2010@hotmail.com>
2017-12-31 16:47:43 +0800

    update reducers definition

--- src/renderer/reducers/conditional.ts
+++ src/renderer/reducers/conditional.ts
@@ -1,60 +1,79 @@
 import * as CONSTANTS from "../constants";
+import IAction from "../interface/IAction";
+import {
+    ISearchConditionState,
+    IFilterConditionState,
+    IOrderConditionState,
+    IGroupConditionState
+} from "../interface/IConditional";
 
-const initialSearchCondition = {
+const searchConditionDefaultState: ISearchConditionState = {
     key: "",
     field: CONSTANTS.SEARCH_FIELD_ALL
 };
 
-export const searchConditionReducer = (state = initialSearchCondition, action) => {
+export const searchConditionReducer = (
+    state = searchConditionDefaultState,
+    action: IAction
+): ISearchConditionState => {
     switch (action.type) {
         case CONSTANTS.UPDATE_SEARCH_CONDITION:
             return {
-                key: action.search.key,
-                field: action.search.field // TODO validation
+                key: action.payload.search.key,
+                field: action.payload.search.field // TODO validation
             };
         default:
             return state;
     }
 };
 
-const initialFilterCondition = {
+const filterConditionDefaultState: IFilterConditionState = {
     hasFlag: false,
     hasNote: false,
     unread: false
 };
 
-export const filterConditionReducer = (state = initialFilterCondition, action) => {
+export const filterConditionReducer = (
+    state = filterConditionDefaultState,
+    action: IAction
+): IFilterConditionState => {
     switch (action.type) {
         case CONSTANTS.UPDATE_FILTER_CONDITION:
-            return action.filter ? action.filter : initialFilterCondition; // TODO validation
+            return action.payload.filter ? action.payload.filter : filterConditionDefaultState; // TODO validation
         default:
             return state;
     }
 };
 
-const initialOrderCondition = {
+const orderConditionDefaultState: IOrderConditionState = {
     by: CONSTANTS.ORDER_BY_DEFAULT,
     desc: true
 };
 
-export const orderConditionReducer = (state = initialOrderCondition, action) => {
+export const orderConditionReducer = (
+    state = orderConditionDefaultState,
+    action: IAction
+): IOrderConditionState => {
     switch (action.type) {
         case CONSTANTS.UPDATE_ORDER_CONDITION:
-            return Object.assign({}, action.order);
+            return Object.assign({}, action.payload.order);
         default:
             return state;
     }
 };
 
-const initialGroupCondition = {
+const groupConditionDefaultState: IGroupConditionState = {
     type: CONSTANTS.GROUP_TYPE_ALL,
     id: CONSTANTS.GROUP_TYPE_ALL // 0
 };
 
-export const groupConditionReducer = (state = initialGroupCondition, action) => {
+export const groupConditionReducer = (
+    state = groupConditionDefaultState,
+    action: IAction
+): IGroupConditionState => {
     switch (action.type) {
         case CONSTANTS.UPDATE_GROUP_CONDITION:
-            return Object.assign({}, action.group);
+            return Object.assign({}, action.payload.group);
         default:
             return state;
     }
